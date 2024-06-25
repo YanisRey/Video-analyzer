9e6d6f28e11b0bc864594b5ccebc6a5f468d74ac
Yanis Merzouki <ymerzouki001@gmail.com>
2024-06-25 15:53:06 -0400

    Set up django project

---
+++ aiBlogGenerator/aiBlogGenerator/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+def homepage(request):
+    return render(request, 'index.html')
+def login(request):
+    return render(request, 'login.html')
+def signup(request):
+    return render(request, 'signup.html')
+def allBlogs(request):
+    return render(request, 'all-blogs.html')
+def blogDetails(request):
+    return render(request, 'blog-details.html')
